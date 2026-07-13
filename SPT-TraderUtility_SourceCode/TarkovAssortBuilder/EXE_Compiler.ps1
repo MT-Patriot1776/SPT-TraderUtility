cc3c2f6b5e43e9be59f91bb81a76e2873a10d0d4
MT-Patriot1776 <cheshierm@gmail.com>
2026-07-12 20:07:51 -0600

    Delete SPT-TraderUtility_SourceCode/TarkovAssortBuilder/EXE_Compiler.ps1

--- SPT-TraderUtility_SourceCode/TarkovAssortBuilder/EXE_Compiler.ps1
+++
@@ -1,8 +0,0 @@
-
-ps2exe -inputFile  'TarkovAssortBuilder_v15.ps1' `
-       -outputFile 'TarkovAssortBuilder.exe' `
-       -x64 -STA -noConsole `
-       -iconFile  'TarkovAssortBuilder.ico' `
-       -title 'Tarkov Assort Builder' `
-       -product 'Tarkov Assort Builder' `
-       -version '1.0.0.0'
